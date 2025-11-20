6e40ee66ba683f9945455c193500fb99478a97fe
Muhammad <kayumovmuhamma@gmail.com>
2025-11-20 23:08:52 +0500

    add: delete board

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
 import json
-from functions.get_board_id import get_board_id
+from typing import Any
+
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import Any
+
+from functions.get_board_id import get_board_id
 
 app = FastAPI()
 
@@ -15,41 +17,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/')
-async def home():
-    return {
-        "message": "success connection"
-    }
 
-@app.get('/board/{id}')
-async def get_todo_data(id: int):
+@app.get("/")
+async def home():
+    return {"message": "success connection"}
+
+
+@app.get("/board/{id}")
+async def get_board(id: int):
     with open("data.json", "r") as file:
         data_json = json.loads(file.read())
 
     return get_board_id(id, data_json["boards"])
 
-@app.get('/boards')
-async def get_todo_data():
+
+@app.get("/boards")
+async def get_boards():
     with open("data.json", "r") as file:
         data_json = json.loads(file.read())
 
     boards = []
 
     for board in data_json["boards"]:
-        boards.append({
-            "title": board["title"],
-            "id": board["id"],
-            "image_url": board["image_url"]
-        })
+        boards.append(
+            {
+                "title": board["title"],
+                "id": board["id"],
+                "image_url": board["image_url"],
+            }
+        )
 
     return boards
+
 
 class DataModel(BaseModel):
     data: Any
     id: int
 
-@app.patch('/board')
-async def edit_todo_data(data: DataModel):
+
+@app.patch("/board")
+async def edit_board(data: DataModel):
     with open("data.json", "r") as file:
         data_json = json.loads(file.read())
         for index, board in enumerate(data_json["boards"]):
@@ -57,11 +64,10 @@ async def edit_todo_data(data: DataModel):
                 data_json["boards"][index]["lists"] = data.data
                 break
 
-    with open("data.json", "w", encoding='utf-8') as file:
+    with open("data.json", "w", encoding="utf-8") as file:
         file.write(json.dumps(data_json, ensure_ascii=False))
 
     return {"message": "OK"}
-
 
 
 class BoardModel(BaseModel):
@@ -79,12 +85,22 @@ async def add_board(board: BoardModel):
             "title": board.title,
             "id": data_json["boardCount"],
             "image_url": board.image_url,
-            "lists": []
+            "lists": [],
         }
     )
 
     data_json["boardCount"] += 1
 
-    with open("data.json", "w", encoding='utf-8') as file:
+    with open("data.json", "w", encoding="utf-8") as file:
         file.write(json.dumps(data_json, ensure_ascii=False))
 
+
+@app.delete("/board")
+async def delete_board(id: int):
+    with open("data.json", "r") as file:
+        data_json = json.loads(file.read())
+
+    data_json.remove(get_board_id(id, data_json))
+
+    with open("data.json", "w", encoding="utf-8") as file:
+        file.write(json.dumps(data_json, ensure_ascii=False))
